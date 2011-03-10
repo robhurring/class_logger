6facb5930646fe3dd5a4f3f7421c995f5ba01e85
Rob Hurring <robert.hurring@lexisnexis.com>
2011-03-10 15:26:13 -0500

    adding some examples

---
+++ examples/examples.rb
@@ -0,0 +1,42 @@
+require File.expand_path('../../lib/class_logger', __FILE__)
+$log_path = File.dirname(__FILE__)
+
+# test module functionality
+module Hello
+  include ClassLogger
+  has_logger :in => "%{class_name}.log", :as => :my_logger
+  
+  def self.alternate
+    loggers[:my_logger]
+  end
+end
+Hello.my_logger.info "Hai"
+Hello.alternate.info "Hai der!"
+
+# test class functionality
+class Gateway
+  include ClassLogger
+  has_logger :path => $log_path, :level => Logger::ERROR
+  has_logger :in => "%{caller_path}/transaction.log", :as => :transaction_logger,
+    :formatter => proc{ |severity, time, program_name, message| "[%s-Transaction]: %s\n" % [severity, message] }
+  
+  def initialize
+    logger.info "Wont show up"
+    logger.error "Will show up"
+  end
+  
+  def transact!
+    transaction_logger.info "Transacted"
+  end
+end
+
+g = Gateway.new
+g.transact!
+
+# test default functionality
+class Default
+  include ClassLogger
+  has_logger :path => "%{caller_path}"
+end
+
+Default.new.logger.info "Testing"
